21fe272d01d05707c4f0fa1607f1d2be6144896a
Gopichand785 <bgopichand3@gmail.com>
2022-07-13 23:44:25 +0530

    okk

--- vijay/assin.js
+++ vijay/assin.js
@@ -7,3 +7,15 @@ console.log(c)*/
 let a=['pro','stack']
 let b=['academy']
 console.log(...a,...b)
+
+//3//highest and longest number in array
+
+let x=[0,20,10,40,602,33,80,326]
+x.sort((x,y)=>y-x)
+console.log(x)
+
+
+let d=[9,5,7,3,1,9,6,3,6,5];
+console.log(d.indexOf(6))
+// let num=[... new set(d)];
+// console.log(num);
